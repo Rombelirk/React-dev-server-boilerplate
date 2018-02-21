001a15c63686ac557cb0da1999e2544001d36e68
Roman <Rombelirk@gmail.com>
2018-02-21 22:08:34 +0300

    story book

---
+++ src/app/components/Layout/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = (props) => {
+    return (
+        <div className="layout-footer-container">
+
+        </div>
+    )
+};
+
+export default Footer;
